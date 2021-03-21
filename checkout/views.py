e6dc3ced4fe46cb54da6affe3755dfe4507b2313
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-21 21:55:00 +0000

    added stripe js and css

--- checkout/views.py
+++ checkout/views.py
@@ -12,8 +12,15 @@ def checkout(request):
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+
+    one = "pk_test_51IXYXtIIkFzlANw7WJOV0f7wFCRmVmIqnH"
+    two = "9lxcwCRkLdMXIcO68gGlV75QCQXLHdhftMLbhNOvlL"
+    three = "gQcdjAiAGHBN003goqSx63"
+    full_key = one + two + three
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': full_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
