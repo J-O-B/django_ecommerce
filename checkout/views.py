e874d5d4f677e2e785dcede0574868244aad1807
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-21 23:52:13 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,14 +12,9 @@ def checkout(request):
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
-
-    one = "pk_test_51IXYXtIIkFzlANw7WJOV0f7wFCRmVmIqnH"
-    two = "9lxcwCRkLdMXIcO68gGlV75QCQXLHdhftMLbhNOvlL"
-    three = "gQcdjAiAGHBN003goqSx63"
-    full_key = one + two + three
     context = {
         'order_form': order_form,
-        'stripe_public_key': full_key,
+        'stripe_public_key': "pk_test_51IXYXtIIkFzlANw7WJOV0f7wFCRmVmIqnH9lxcwCRkLdMXIcO68gGlV75QCQXLHdhftMLbhNOvlLgQcdjAiAGHBN003goqSx63",
         'client_secret': 'test client secret',
     }
 
