04515b6b7a9edad6603fc3edd6bc4a7f42dcb889
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-24 01:54:55 +0000

    home page modelling, added js, icon slider, css and JS for slider and title + icons

--- static/js/home.js
+++ static/js/home.js
@@ -1,19 +1,67 @@
 $(document).ready(function(){
-    $('#about-us').hide();
+
+    if($('.brands_slider').length){
+        let brandsSlider = $('.brands_slider');
+
+        brandsSlider.owlCarousel({
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:5000,
+            nav:false,
+            dots:false,
+            autoWidth:true,
+            items:8,
+            margin:42
+        });
+
+        if($('.brands_prev').length){
+            let prev = $('.brands_prev');
+            prev.on('click', function(){
+            brandsSlider.trigger('prev.owl.carousel');
+            });
+        }
+
+        if($('.brands_next').length){
+            let next = $('.brands_next');
+            next.on('click', function(){
+                brandsSlider.trigger('next.owl.carousel');
+            });
+        }
+    }
+    // Initial Visibilities
     $('#security').hide();
     $('#prodRange').hide();
-})
+    $('#howItWorks').hide();
+    $('#page-content').hide();
+});
 
+$(window).scroll(function() {
+    let top_of_element = ($("#page-content").offset().top) + 500;
+    let bottom_of_element = $("#page-content").offset().top + $("#page-content").outerHeight();
+    let bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    let top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        $('#page-content').fadeIn(1500);
+    } else {
+        // Pass
+    }
+});
+
+
+// Click functions
 $('#showabout').click(function(){
-    $('#about-us').siblings().hide(1000);
-    $('#about-us').show(1000);
-})
+    $('#about-us').siblings().hide(900).then($('#about-us').fadeIn(500));    
+});
 $('#showsecurity').click(function(){
-    $('#security').siblings().hide(1000);
-    $('#security').show(1000);
-})
+    $('#security').siblings().hide(900).then($('#security').fadeIn(900));
+});
 $('#showProdRange').click(function(){
-    $('#prodRange').siblings().hide(1000);
-    $('#prodRange').show(1000);
-})
-    
+    $('#prodRange').siblings().hide(900).then($('#prodRange').fadeIn(900));
+});
+$('#showHow').click(function(){
+    $('#howItWorks').siblings().hide(900).then($('#howItWorks').fadeIn(900));
+});
+$('.show-how-it-works').click(function(){
+    $('#showHow').trigger('click');
+});
