1c3b45928fc931eb62770190e101f109decf11d1
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-25 00:16:13 +0000

    added JS and python to scrape checkout info to feed back to database to deal with checkout issues

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,7 +20,76 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.data.charges[0].ammount / 100, 2)
+
+        # Clean data in the shipping details
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        try:
+            order = Order.objects.get(
+                full_name_iexact=shipping_details.name,
+                email_iexact=shipping_details.email,
+                phone_number_iexact=shipping_details.phone,
+                country_iexact=shipping_details.country,
+                postcode_iexact=shipping_details.postal_code,
+                town_or_city_iexact=shipping_details.city,
+                street_address1_iexact=shipping_details.line1,
+                street_address2_iexact=shipping_details.line2,
+                county_iexact=shipping_details.state,
+                grand_total=grand_total,
+            )
+            order_exists = True
+            one = f'Webhook received: {event["type"]} '
+            two = '| SUCCESS: Verified order already in database'
+            full = one + two
+            return HttpResponse(content=full, status=200)
+        except Order.DoesNotExist:
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=shipping_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.country,
+                    postcode=shipping_details.postal_code,
+                    town_or_city=shipping_details.city,
+                    street_address1=shipping_details.line1,
+                    street_address2=shipping_details.line2,
+                    county=shipping_details.state,
+                )
+                for item_id, item_data in json.loads(bag).items():
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} ERROR: {e}',
+                    status=500)
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
