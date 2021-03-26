e25ac3f82c80158d7d1c9e4ca49d0461a51308d2
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-26 23:54:15 +0000

    added the email template and attached to webhook

--- store/settings.py
+++ store/settings.py
@@ -180,3 +180,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+# Default Email
+
+DEFAULT_FROM_EMAIL = 'Admin@Giburoo.com'
