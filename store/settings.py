b3468ffe18d4da1751465b26e5b0a3285096d989
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-28 19:09:43 +0000

    hid secret key

--- store/settings.py
+++ store/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "p9naqbt33)m=x&g=x0mv$&@$a@!4@$td2=39s0%4@ghr^wod_t"
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['django-store-test.herokuapp.com', 'localhost']
 
